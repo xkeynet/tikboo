7b1d40127d56d5a3aca100720b6c4ba8630d53ea
XKEYNET <xkeynet@gmail.com>
2026-05-03 23:03:12 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,9 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const PLAYLIST = [
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/swipe0.mp4' },
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/swipe.mp4' },
-    { type: 'video', src: 'assets/video/swipe1.mp4' },
-    { type: 'video', src: 'assets/video/swipe2.mp4' },
-    { type: 'video', src: 'assets/video/swipe3.mp4' },
+    { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/swipe1.mp4' },
+    { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/swipe2.mp4' },
+    { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/swipe3.mp4' },
     { type: 'video', src: 'assets/video/swipe4.mp4' },
     { type: 'video', src: 'assets/video/swipe5.mp4' },
     { type: 'video', src: 'assets/video/swipe6.mp4' },
