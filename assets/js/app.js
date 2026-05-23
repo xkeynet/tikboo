43486a00f7d74556d9b555f164e1046c48e2b148
XKEYNET <xkeynet@gmail.com>
2026-05-23 06:51:48 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,8 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // === State / Playlist ===
   // =========================================================
   const PLAYLIST = [
-    { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/%23beatbox.mp4' },
-    { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/BradPitt.mp4' },
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/Millennial.mp4' },
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/GeorginaRodriguez.mp4' },
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/getting-ready.mp4' },
