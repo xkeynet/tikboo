a0c6c323faf9a9abfb4063c53cff20e1e9f5e987
XKEYNET <xkeynet@gmail.com>
2026-05-27 19:40:29 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // === State / Playlist ===
   // =========================================================
   const PLAYLIST = [
-    { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/Ass.mp4' },
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/Loving.mp4' },
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/haha.mp4' },
     { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/Fav.mp4' },
