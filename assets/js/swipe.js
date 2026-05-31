b971ae8219d718b53d03da38af007101ba60137a
XKEYNET <xkeynet@gmail.com>
2026-06-01 00:03:02 +0200

    Update swipe.js

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -278,7 +278,7 @@
         setTimeout(() => {
           if (!state.isAnimating) return;
           tryPlay(targetVideo);
-        }, 45);
+        }, 25);
       }
 
       refs.layerCurrent.style.willChange = 'transform';
