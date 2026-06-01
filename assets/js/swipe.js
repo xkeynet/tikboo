10056a277d490b0f1aec38706012e8574917e37f
XKEYNET <xkeynet@gmail.com>
2026-06-01 22:04:27 +0200

    Update swipe.js

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -369,13 +369,14 @@
         resetQueue();
 
         requestAnimationFrame(() => {
+          if (queued !== 0 && !state.isAnimating) {
+            prepareNextForDirection(queued);
+            commit(queued);
+            return;
+          }
+
           warmForwardNext();
           warmBackwardNext();
-
-          if (queued !== 0 && !state.isAnimating) {
-            preparedDir = queued;
-            commit(queued);
-          }
         });
       }, duration); 
     }
