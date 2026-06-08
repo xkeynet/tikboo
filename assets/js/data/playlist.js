6d6b9da47656da425db81f4a8bbbe11b8c72f5d6
XKEYNET <xkeynet@gmail.com>
2026-06-08 21:02:05 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -1,6 +1,7 @@
 // /assets/js/data/playlist.js
 
 export const PLAYLIST = [
+  { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/hottmesscrystal_85.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/7thsunday.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/lianalovexo.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/%23highheels.mp4' },
