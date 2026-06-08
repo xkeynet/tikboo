84d74e6e7847a0d86aa78125bcd0e301147651f2
XKEYNET <xkeynet@gmail.com>
2026-06-08 11:38:53 +0200

    Update supabaseClient.js

--- assets/js/utils/supabaseClient.js
+++ assets/js/utils/supabaseClient.js
@@ -1,1 +1,11 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
+const supabaseUrl = 'https://wnxzeklcoxjyfkagbfvm.supabase.co'
+
+const supabaseKey =
+  'sb_publishable_sBBZErFYXJ7h1t6v-H4aYQ_UUPMvusG'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
