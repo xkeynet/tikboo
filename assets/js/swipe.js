480ef67b702ef69154e7b0c6abab66a9de7cb04e
XKEYNET <xkeynet@gmail.com>
2026-06-09 10:19:46 +0200

    Update swipe.js

--- assets/js/swipe.js
+++ assets/js/swipe.js
@@ -7,16 +7,16 @@
       ensureSoundOn, isInteractiveTarget 
     } = options;
 
-    const THRESHOLD_RATIO = 0.13; 
+    const THRESHOLD_RATIO = 0.25; 
     const MOVE_ACTIVATE_PX = 3;    
-    const MIN_COMMIT_DY = 38;      
-    const MIN_COMMIT_VY = 0.34;    
+    const MIN_COMMIT_DY = 70;      
+    const MIN_COMMIT_VY = 0.42;    
     const TAP_MAX_MOVE = 8;
     const TAP_MAX_TIME = 220;
 
-    const BACKWARD_THRESHOLD_RATIO = 0.09;
-    const BACKWARD_MIN_COMMIT_DY = 30;
-    const BACKWARD_MIN_COMMIT_VY = 0.24;
+    const BACKWARD_THRESHOLD_RATIO = 0.18;
+    const BACKWARD_MIN_COMMIT_DY = 55;
+    const BACKWARD_MIN_COMMIT_VY = 0.34;
 
     const DIRECTION_FLIP_DAMPING_PX = 12;
     const QUEUE_MOVE_ACTIVATE_PX = 2;
