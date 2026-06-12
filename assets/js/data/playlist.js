9f72fece6af09f30c6890f09f3ed7e11351379d6
XKEYNET <xkeynet@gmail.com>
2026-06-12 02:08:37 +0200

    Add new video to the playlist

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -1,6 +1,7 @@
 // /assets/js/data/playlist.js
 
 export const PLAYLIST = [
+  { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/Nikki.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/totally.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/Energy.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/AQ.mp4' },
