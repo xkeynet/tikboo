3200aa9fdf430e4bb7ca5ecf6efadd26aa381cbf
XKEYNET <xkeynet@gmail.com>
2026-06-13 10:51:20 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -1,6 +1,9 @@
 // /assets/js/data/playlist.js
 
 export const PLAYLIST = [
+  { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/mosaffari.lambo.mp4' },
+  { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/breeafterhrss.pool.mp4' },
+  { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/worshipbunnii.atm.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/kylekrieger.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/sofi.sun.s.mp4' },
   { type: 'video', src: 'https://pub-dcf634f0c29b4449bae68897ac703aff.r2.dev/videos/insta/Yacht.mp4' },
