bda39a1358d5223e9c73e42531706adafa57b7c8
XKEYNET <xkeynet@gmail.com>
2026-07-01 00:19:41 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -1,7 +1,7 @@
 // /assets/js/data/playlist.js
 
 export const PLAYLIST = [
-  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/julianomass-002.mp4', creator: 'julianomass', caption: "I'm bustin' mad moves to El Farsante", followUrl: 'https://www.instagram.com/julianomass/' },
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/julianomass-002.mp4', creator: 'julianomass', caption: "I'm bustin' mad moves to El Farsante", followUrl: 'https://www.instagram.com/julianomass/', avatar: '/assets/avatars/julianomass.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/mayaalessiaaaa-001.mp4', creator: 'mayaalessiaaaa', caption: 'My pretty face 💖', followUrl: 'https://www.instagram.com/mayaalessiaaaa/' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ripsigal-001.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/daddysjuiced_-003.mp4' },
