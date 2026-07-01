02c385addfbf7e113f4bb818a4b8dd35aa666949
XKEYNET <xkeynet@gmail.com>
2026-07-01 21:26:49 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -6,7 +6,7 @@ export const PLAYLIST = [
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ripsigal-001.mp4', creator: 'ripsigal', caption: 'Main character energy..🥤', followUrl: 'https://www.instagram.com/ripsigal/', avatar: '/assets/avatars/ripsigal.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/daddysjuiced_-003.mp4', creator: 'daddysjuiced_', caption: "Got me too wet babyyy!", followUrl: 'https://www.instagram.com/daddysjuiced_/', avatar: '/assets/avatars/daddysjuiced_.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/laurenwolfe-001.mp4', creator: 'laurenwolfe', caption: "Best Mannequin Challenge in a girls' world 💅", followUrl: 'https://www.instagram.com/laurenwolfe/', avatar: '/assets/avatars/laurenwolfe.jpg' },
-  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/liifewniic-001.mp4' },
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/liifewniic-001.mp4', creator: 'liifewniic', caption: "Dad: Where are you going?", followUrl: 'https://www.instagram.com/liifewniic/', avatar: '/assets/avatars/liifewniic.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/daddysjuiced_-001.mp4', creator: 'daddysjuiced_', caption: "Happy Father's Day yall we love you!", followUrl: 'https://www.instagram.com/daddysjuiced_/', avatar: '/assets/avatars/daddysjuiced_.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ellzz22x.mp4', creator: 'ellzz22x', caption: "Just at home dancing in the mirror..lol", followUrl: 'https://www.instagram.com/ellzz22x/', avatar: '/assets/avatars/ellzz22x.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ripsigal-002.mp4' },
