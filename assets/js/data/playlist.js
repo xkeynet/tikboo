30702596acbe865c96f44019f1c4ae427d26637b
XKEYNET <xkeynet@gmail.com>
2026-07-03 03:54:46 +0200

    Enhance video entry with creator and caption
    Added creator and caption details for a video in the playlist.

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -11,7 +11,7 @@ export const PLAYLIST = [
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/daddysjuiced_-001.mp4', creator: 'daddysjuiced_', caption: "Happy Father's Day yall we love you!", followUrl: 'https://www.instagram.com/daddysjuiced_/', avatar: '/assets/avatars/daddysjuiced_.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ellzz22x.mp4', creator: 'ellzz22x', caption: "Just at home dancing in the mirror..lol", followUrl: 'https://www.instagram.com/ellzz22x/', avatar: '/assets/avatars/ellzz22x.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ripsigal-002.mp4', creator: 'ripsigal', caption: "Vibe check in the parking lot! 🎧", followUrl: 'https://www.instagram.com/ripsigal/', avatar: '/assets/avatars/ripsigal.jpg' },
-  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/rafaeltats-001.mp4' },
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/rafaeltats-001.mp4', creator: 'rafaeltats', caption: "You really wanna do that? It's a done deal bro 💵", followUrl: 'https://www.instagram.com/rafaeltats/', avatar: '/assets/avatars/rafaeltats.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/rafaeltatsclips-001.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/onlysophielouisee-001.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/nikkisandiego-001.mp4' },
