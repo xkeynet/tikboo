eacdbff14904bd03117c6344ecfc9ddf2f2b0d5a
XKEYNET <xkeynet@gmail.com>
2026-07-04 08:59:16 +0200

    Enhance Kila video entry with additional metadata
    Added creator, caption, followUrl, and avatar to the Kila video entry.

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -203,7 +203,7 @@ export const PLAYLIST = [
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/Thoughts.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/MiamiBeach.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/sydneylint.mp4' },
-  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/Kila.mp4' },
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/Kila.mp4', creator: 'lopirova', caption: "Чиллим дома", followUrl: 'https://www.instagram.com/lopirova/', avatar: '/assets/avatars/lopirova.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/rebeccamcleoddd-66.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/rebeccamcleoddd-67.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/Tiger.mp4', creator: 'daddysjuiced_', caption: "Eye of the Tiger", followUrl: 'https://www.instagram.com/daddysjuiced_/', avatar: '/assets/avatars/daddysjuiced_.jpg' },
