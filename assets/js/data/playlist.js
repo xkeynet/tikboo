0b2ff967936e45911956fe18ad7a3f3c4902e67c
XKEYNET <xkeynet@gmail.com>
2026-07-07 17:08:01 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -29,7 +29,7 @@ export const PLAYLIST = [
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/lady_s_dreamer-001.mp4', creator: 'lady_s_dreamer', caption: "Sono l'unica a cui manca disperatamente l'estate", followUrl: 'https://www.instagram.com/lady_s_dreamer/', avatar: '/assets/avatars/lady_s_dreamer.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ripsigal-005.mp4', creator: 'ripsigal', caption: "Now You're Gone ✈️", followUrl: 'https://www.instagram.com/ripsigal/', avatar: '/assets/avatars/ripsigal.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/angell_novikova-001.mp4', creator: 'angell_novikova', caption: "When you and your bestie finally got here", followUrl: 'https://www.instagram.com/angell_novikova/', avatar: '/assets/avatars/angell_novikova.jpg' },
-  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ripsigal-006.mp4' },
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ripsigal-006.mp4', creator: 'ripsigal', caption: "Не отстаем от трендов 🤪🤘", followUrl: 'https://www.instagram.com/ripsigal/', avatar: '/assets/avatars/ripsigal.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ika_dauria-001.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/mariadmsanchez-001.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/violeta_london-001.mp4' },
