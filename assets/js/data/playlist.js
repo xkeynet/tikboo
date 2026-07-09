9adfb54ef9d757f5e998ee49c238d59e851020d2
XKEYNET <xkeynet@gmail.com>
2026-07-09 07:01:29 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -1,6 +1,7 @@
 // /assets/js/data/playlist.js
 
 export const PLAYLIST = [
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/julianomass-008.mp4', creator: 'julianomass', caption: "So locked @ultrapouches", followUrl: 'https://www.instagram.com/julianomass/', avatar: '/assets/avatars/julianomass.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/JackDoherty.mp4', creator: 'jackdoherty', caption: "Dream life 💎 #jackdoherty", followUrl: 'https://www.instagram.com/jackdoherty/', avatar: '/assets/avatars/jackdoherty.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/jackdoherty-003.mp4', creator: 'jackdoherty', caption: "What you saying", followUrl: 'https://www.instagram.com/jackdoherty/', avatar: '/assets/avatars/jackdoherty.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/jackdoherty-002.mp4', creator: 'jackdoherty', caption: "I’m the reason she know how a jet feel 🛩️", followUrl: 'https://www.instagram.com/jackdoherty/', avatar: '/assets/avatars/jackdoherty.jpg' },
