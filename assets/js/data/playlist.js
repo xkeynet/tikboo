c9e27f733809d137a0a1faf50e9e62b4dcd70a24
XKEYNET <xkeynet@gmail.com>
2026-07-09 07:24:49 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -1,6 +1,7 @@
 // /assets/js/data/playlist.js
 
 export const PLAYLIST = [
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/emjayrinaudo-011.mp4', creator: 'emjayrinaudo', caption: "Day 1 in Tokyo 🇯🇵 Spent too much on Chrome", followUrl: 'https://www.instagram.com/emjayrinaudo/', avatar: '/assets/avatars/emjayrinaudo.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/julianomass-008.mp4', creator: 'julianomass', caption: "So locked @ultrapouches", followUrl: 'https://www.instagram.com/julianomass/', avatar: '/assets/avatars/julianomass.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/JackDoherty.mp4', creator: 'jackdoherty', caption: "Dream life 💎 #jackdoherty", followUrl: 'https://www.instagram.com/jackdoherty/', avatar: '/assets/avatars/jackdoherty.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/jackdoherty-003.mp4', creator: 'jackdoherty', caption: "What you saying", followUrl: 'https://www.instagram.com/jackdoherty/', avatar: '/assets/avatars/jackdoherty.jpg' },
