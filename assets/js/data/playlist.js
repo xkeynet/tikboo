e31d5b2d7039d4427891e3a1004494a519b3e4ef
XKEYNET <xkeynet@gmail.com>
2026-07-15 10:01:52 +0200

    Update playlist.js

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -197,7 +197,7 @@ export const PLAYLIST = [
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/Sunday.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/DKR.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/ONEE.mp4' },
-  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/Beach.mp4' },
+  { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/Beach.mp4', creator: 'hayleydavies.bts', caption: "Think you can keep up? 🫦", followUrl: 'https://www.instagram.com/hayleydavies.bts/', avatar: '/assets/avatars/hayleydavies.bts.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/JLO.mp4', creator: 'jlo', caption: "Jennifer Lopez", followUrl: 'https://www.instagram.com/jlo/', avatar: '/assets/avatars/jlo.jpg' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/King.mp4' },
   { type: 'video', src: 'https://cdn.tikboo.com/videos/insta/reecemariexx.mp4' },
