4cf62236396c12b3c6175c5daac133d709c74ab6
XKEYNET <xkeynet@gmail.com>
2026-08-13 06:12:43 +0200

    Add Jake Paul video entry to PLAYLIST
    Added a new video entry to the PLAYLIST with details for creator Jake Paul.

--- assets/js/data/playlist.js
+++ assets/js/data/playlist.js
@@ -2,6 +2,13 @@
 
 export const PLAYLIST = [
   { type: 'video', 
+  manifest: 'https://customer-akbn1e8h41lg80xg.cloudflarestream.com/d9159f2cc4a9e76dfe4592d0ced2f13f/manifest/video.m3u8', 
+  creator: 'jakepaul', 
+  caption: "@jakepaul", 
+  followUrl: 'https://www.instagram.com/jakepaul/', 
+  avatar: 'https://cdn.tikboo.com/avatars/jakepaul.jpg' },
+  
+  { type: 'video', 
   manifest: 'https://customer-akbn1e8h41lg80xg.cloudflarestream.com/559d20c3c27a323453ca79d790d969db/manifest/video.m3u8', 
   creator: 'campuzanoangie', 
   caption: "@campuzanoangie", 
