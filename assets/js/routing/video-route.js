0a82c043662a3feb07fb0e1eea885922427dfa56
XKEYNET <xkeynet@gmail.com>
2026-08-15 05:09:08 +0200

    Update video-route.js

--- assets/js/routing/video-route.js
+++ assets/js/routing/video-route.js
@@ -1,1 +1,11 @@
+// /assets/js/routing/video-route.js
 
+export function getVideoIdFromUrl() {
+  const match = window.location.pathname.match(/^\/v\/([^/]+)\/?$/);
+
+  if (!match) {
+    return null;
+  }
+
+  return decodeURIComponent(match[1]);
+}
