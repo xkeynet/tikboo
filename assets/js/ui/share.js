18b4b0ae683afcb59efb7f402c29f153371e41da
XKEYNET <xkeynet@gmail.com>
2026-08-15 05:24:38 +0200

    Implement video sharing functions in share.js
    Add functions to generate and share video URLs

--- assets/js/ui/share.js
+++ assets/js/ui/share.js
@@ -1,1 +1,29 @@
+// /assets/js/ui/share.js
 
+const TIKBOO_ORIGIN = 'https://tikboo.com';
+
+export function getVideoShareUrl(video) {
+  return `${TIKBOO_ORIGIN}/v/${video.id}`;
+}
+
+export async function copyVideoShareUrl(video) {
+  const url = getVideoShareUrl(video);
+
+  await navigator.clipboard.writeText(url);
+
+  return url;
+}
+
+export async function openNativeVideoShare(video) {
+  const url = getVideoShareUrl(video);
+
+  const shareData = {
+    title: 'Tikboo',
+    text: 'Watch this',
+    url
+  };
+
+  await navigator.share(shareData);
+
+  return url;
+}
